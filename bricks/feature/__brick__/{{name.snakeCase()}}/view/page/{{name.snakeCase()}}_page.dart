d733df773362e511e3dccaf482fed371eda8bda2
Sugith K <134681105+sugith10@users.noreply.github.com>
2025-11-14 22:40:14 +0530

    feature brick

---
+++ bricks/feature/__brick__/{{name.snakeCase()}}/view/page/{{name.snakeCase()}}_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class {{name.pascalCase()}}Page extends StatelessWidget {
+  const {{name.pascalCase()}}Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          
+        ],
+      ),
+    );
+  }
+}
